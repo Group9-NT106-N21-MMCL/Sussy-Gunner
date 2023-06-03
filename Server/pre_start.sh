1fb6ee47ac09d772602d98aef2a355492f9b7759
t0obUnR34L <khocvole1kg3@gmail.com>
2023-06-03 08:42:38 +0700

    fix db

--- Server/pre_start.sh
+++ Server/pre_start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-echo "Downloading database cert..."
+set -eu
+
+echo "Preparing to Fly..."
+
+echo "Fetching cluster cert..."
 curl -o /nakama/db.crt "https://cockroachlabs.cloud/clusters/$CLUSTER_ID/cert"
 
 echo "Migrating database..."
 /nakama/nakama migrate up --database.address "$DATABASE_URL"
-
--- Server/start.sh
+++ Server/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-echo "Pre starting..."
-/bin/sh /nakama/pre_start.sh
+set -eu
 
-echo "Running Nakama..."
-exec /nakama/nakama --name nakama1 --database.address "$DATABASE_URL" --logger.level DEBUG --session.token_expiry_sec 7200 --metrics.prometheus_port 9091
+sh /nakama/pre_start.sh
 
+echo "Starting to Fly..."
+exec /nakama/nakama --name nakama1 \
+  --database.address "$DATABASE_URL" \
+  --logger.level DEBUG \
+  --session.token_expiry_sec 7200 \
+  --metrics.prometheus_port 9091
