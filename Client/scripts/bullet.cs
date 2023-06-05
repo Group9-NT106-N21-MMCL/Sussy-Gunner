fa1c7348aef246629032f2f77e1b5aa4877fbdac
MurazeKrinas <vanlinh.1942003@gmail.com>
2023-06-06 04:05:55 +0700

    Fix bug

--- Client/scripts/bullet.cs
+++ Client/scripts/bullet.cs
@@ -7,26 +7,23 @@ using Nakama.TinyJson;
 
 public partial class bullet : CharacterBody2D
 {
-	private ClientNode ClientNode => this.Autoload<ClientNode>();
-	float maxRange = 1000;
-	float distanceTravelled = 0;
-	public float damage = 20;
-	float speed = 1500;
-	private IMatch match;
-	public void SetMatch(IMatch X) => match = X;
-	public override void _Process(double delta)
-	{
-		float moveAmount = (float)(speed * delta);
-		Position += Transform.X.Normalized() * (float)moveAmount;
-		distanceTravelled += moveAmount;
-		if (distanceTravelled > maxRange)
-			QueueFree();
-	}
+    float maxRange = 1000;
+    float distanceTravelled = 0;
+    public float damage = 20;
+    float speed = 1500;
+    public override void _Process(double delta)
+    {
+        float moveAmount = (float)(speed * delta);
+        Position += Transform.X.Normalized() * (float)moveAmount;
+        distanceTravelled += moveAmount;
+        if (distanceTravelled > maxRange)
+            QueueFree();
+    }
 
-	public void _on_area_2d_area_entered(Area2D area)
-	{
-		if (area.Name == "WallArea" || area.Name == "Area")
-			QueueFree();
-	}
+    public void _on_area_2d_area_entered(Area2D area)
+    {
+        if (area.Name == "WallArea" || area.Name == "Area")
+            QueueFree();
+    }
 }
 
