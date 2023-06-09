8d969017c62dfce7b98983e0f918faf4a85eaa6c
TooBunReal <khocvole1kg3@gmail.com>
2023-06-09 20:43:48 +0700

    big update server

--- Server/start.sh
+++ Server/start.sh
@@ -4,8 +4,17 @@ set -eu
 
 sh /nakama/pre_start.sh
 
+# sh /ts/start.sh
+
+# TS_IP=$(/ts/tailscale ip -4)
+
 echo "Starting to Fly..."
-exec /nakama/nakama --name nakama1 \
+
+# cd /nakama/
+# mkdir -p data/modules
+# mv index.js data/modules
+
+exec /nakama/nakama --name sussy-gunner1 \
   --database.address "$DATABASE_URL" \
   --logger.level DEBUG \
   --session.token_expiry_sec 7200 \
